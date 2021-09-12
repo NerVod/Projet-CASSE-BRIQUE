816d3fb881937d7022dfddf70e10ae905743ee32
Benjamin JEANNEROT <benjamin.jeannerot@gmail.com>
2021-09-12 14:37:55 +0200

    ajout balle + grilles de briques+ texte niveau

---
+++ data/levelMaps.js
@@ -0,0 +1,28 @@
+let levels = [
+        [
+    [
+        "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue",
+    ],
+    [
+        "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue",
+    ],
+    [
+        "red", "red", "red", "red", "red", "red", "red", "red", "red", "red", 
+    ],
+    [
+        "red", "red", "red", "red", "red", "red", "red", "red", "red", "red",
+    ],
+    [
+        "gold", "gold", "gold", "gold", "gold", "gold", "gold", "gold", "gold", "gold", 
+    ],
+    [
+        "gold", "gold", "gold", "gold", "gold", "gold", "gold", "gold", "gold", "gold",
+    ],
+    [
+        "green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,
+    ],
+    [
+        "green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,"green" ,
+    ],
+        ]
+];
